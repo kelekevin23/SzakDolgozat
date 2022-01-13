61af91aa1ee37ccb6402cd5d4a571817a9fac764
kelekevin23 <kelekevin11@gmail.com>
2022-01-14 00:18:52 +0100

    Szakdolgozat elkezdése

---
+++ Kapcsolat.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Szakdolgozat</title>
+        <script src="js/jquery-3.6.0.min.js"></script>
+        <script src="js/proba.js"></script>
+        <script src="js/script.js"></script>
+        <script src="js/galeria.js"></script>
+        <link rel="stylesheet" href="css/szerkezet.css">
+    </head>
+
+    <body>
+
+        <main>
+
+            <div class="header-container">
+                <header>
+                    <h1>B-Shop</h1>
+                    <input type="text" placeholder="Keresés..." name="kereso" , id="kereso">
+                    <div id="panel">
+                        <button id="belepes">Belépés</button>
+                        <button id="regisztracio">Regisztráció</button>
+                    </div>
+                </header>
+
+                <nav>
+                    <div id="menu">
+                        <li><a href="index.php">Főoldal</a></li>
+                        <li><a href="borondok.php">Bőröndök</a></li>
+                        <li><a href="Kapcsolat.php">Kapcsolat</a></li>
+                        <li><a href="kosar.php">Kosár</a></li>
+                    </div>
+                </nav>
+            </div>
+
+            <article>
+                
+
+            </article>
+
+            <section>
+                
+            </section>
+
+            <aside class="adatok">
+
+            </aside>
+
+            <footer>
+
+            </footer>
+        </main>
+
+
+    </body>
+
+</html>
